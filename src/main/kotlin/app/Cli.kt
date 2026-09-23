91b93b6013a5d0f5b7725af8424bc76f28095619
YajAnh <267958742+YajAnh@users.noreply.github.com>
2026-09-23 20:41:27 +0800

    Initial implementation of extended sorting rules

--- src/main/kotlin/app/Cli.kt
+++ src/main/kotlin/app/Cli.kt
@@ -1,28 +1,34 @@
 package app
 
-import rules.homeDirectory
-import rules.sortingRules
-
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.boolean
-
 import config.loadConfig
 import config.loadDirectories
 import config.loadHistory
 import config.saveConfig
-import sorting.ensureFolderDestination
-import sorting.sortingLogic
+import kotlinx.datetime.LocalDate
+import rules.DateDataGroup
+import rules.Destination
+import rules.ExtensionsData
+import rules.NameLengthDataGroup
+import rules.PatternData
+import rules.SizeDataGroup
+import rules.homeDirectory
+import rules.sortingRules
 import services.clearHistory
 import services.formatSessionTimestamp
 import services.undoLogic
 import services.undoLogicDryRun
+import sorting.ensureFolderDestination
 import sorting.ensureFolderDestinationDryRun
 import sorting.sortingDryRun
+import sorting.sortingLogic
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.isDirectory
@@ -51,6 +57,128 @@ class App : CliktCommand(name = "aurum-rc") {
 
 class Sort : CliktCommand(name = "sort", help = "Sort files in a directory") {
     private val folder by argument(help = "Standard folder name or path to sort").optional()
+    //priority field will be chronological, depending on option is entered first and so on
+
+    private val extensions by option("--extensions", help = "Add sorting rule to sort extensions")
+        .convert {
+            ExtensionsData(it)
+        }
+    // --extensions jpg,jpeg,png,gif,webm
+    // --extensions mp4,mov
+
+
+    private val date by option("--date", help = "Add sorting rule to sort depending on the date")
+        .convert {it ->
+            val dateInputParsed = it.trim().split(Regex("\\s+"))
+            val dateOption = when (dateInputParsed[0]) {
+                "created" -> DateDataGroup.DateOptionEnum.CREATED
+                "modified" -> DateDataGroup.DateOptionEnum.MODIFIED
+                "age" -> DateDataGroup.DateOptionEnum.AGE
+                else -> throw IllegalArgumentException("Unexpected option type ${dateInputParsed[0]}")
+            }
+
+            if (dateOption == DateDataGroup.DateOptionEnum.AGE) {
+                val ageComparison = when (dateInputParsed[1]) {
+                        "older" -> DateDataGroup.AgeEnum.OLDER
+                        "newer" -> DateDataGroup.AgeEnum.NEWER
+                        else -> throw IllegalArgumentException("Unexpected age comparison type ${dateInputParsed[1]}")
+                    }
+
+
+
+                val ageDateEntries = dateInputParsed[2].split(":")
+                return@convert DateDataGroup.AgeData(
+                    dateOption,
+                    ageComparison,
+                    ageDateEntries
+                )
+            }
+
+            val dateSections = dateInputParsed[1].split(":")
+            DateDataGroup.DateData(
+                dateOption,
+                dateSections.map { LocalDate.parse(it) }
+            )
+
+        }
+    // --date created YYYY-MM-DD:YYYY-MM-DD
+    // --date modified
+    // (YYYY/MM/DD)  age older/newer 1m:10d (older == files older than 1m and 10d, newer == files newer than 1m and 10d)
+
+    private val nameLength by option("--length", help = "Add sorting rule to sort depending on the file name length")
+        .convert { it ->
+            val nameLengthInputParse = it.trim().split(Regex("\\s+"))
+
+            require(nameLengthInputParse.size == 2) {
+                "--length requires: <less|greater|range|exact> <value>"
+            }
+
+            val nameLengthOption = when (nameLengthInputParse[0]) {
+                    "less" -> NameLengthDataGroup.NameLengthComparisonStatementsEnum.LESS
+                    "greater" -> NameLengthDataGroup.NameLengthComparisonStatementsEnum.GREATER
+                    "range" -> NameLengthDataGroup.NameLengthComparisonStatementsEnum.RANGE
+                    "exact" -> NameLengthDataGroup.NameLengthComparisonStatementsEnum.EXACT
+                    else -> throw IllegalArgumentException("Unexpected name length type ${nameLengthInputParse[0]}")
+                }
+
+            val ranges = if (nameLengthInputParse[1].contains("-")) {
+                nameLengthInputParse[1].split("-").map {
+                    it.toInt()
+                }
+            } else {
+                listOf(nameLengthInputParse[1].toInt())
+            }
+            NameLengthDataGroup.NameLengthData(nameLengthOption, ranges)
+        }
+    // --length less 10 ~ LESS THAN
+    // --length greater 5 ~ GREATER THAN
+    // --length range 10-5 ~ RANGE
+    // --length 10 EXACT
+
+    private val size by option("--size", help = "Add sorting rule to sort depending on size")
+        .convert {
+            val sizeInputParsed = it.trim().split(Regex("\\s+"))
+            if (sizeInputParsed[0] !in listOf("less", "greater")) {
+                val getSize = sizeInputParsed[0].split("-")
+
+                SizeDataGroup.Range(getSize)
+                return@convert
+            }
+
+            val comparisonStatement = run {
+                when (sizeInputParsed[0]) {
+                    "less" -> SizeDataGroup.SizeComparisonStatementsEnum.LESS
+                    "greater" -> SizeDataGroup.SizeComparisonStatementsEnum.GREATER
+                    else -> throw error("Unreachable")
+                }
+            }
+            val getSize = sizeInputParsed[1]
+            SizeDataGroup.CompareData(comparisonStatement, getSize)
+            return@convert
+        }
+    // --size 1mb-100mb ~ less 100mb ~ greater 50mb
+
+    private val pattern by option("--pattern", help = "Add sorting rule to sort depending on prefixes/pattern that matches a file")
+        .convert {
+            val patternInputParsed = it.trim().split(Regex("\\s+"))
+            val patterns = patternInputParsed[0].split(",")
+            PatternData(patterns)
+        }
+    // --pattern screenshot*,IMG*
+
+    private val destination by option("--destination", help = "Custom destination of the file at instance")
+        .convert {
+            val destinationInputParsed = it.trim().split(Regex("\\s+"))
+            val name = destinationInputParsed[0]
+            runCatching {
+                val path = Path.of(destinationInputParsed[1])
+                Destination(name, path)
+            }.getOrThrow()
+
+
+        }
+    // --destination <Name> <Path>
+
     private val pinned by option("--pinned", help = "Sort files in a pinned directory (by name)")
     private val dryRun by option("--dryrun", help = "Shows what files get moved, without moving").flag()
 
